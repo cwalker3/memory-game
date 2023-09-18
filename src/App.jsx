12786e980aa0306dfc8e9839e41dcd855d29f92b
Cory Walker <cory.walker.dev@gmail.com>
2023-09-18 10:07:17 -0700

    Add StartScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
+import StartScreen from './components/StartScreen';
 
 function App() {
 
   return (
     <>
-    
+      <StartScreen />
     </>
   )
 }
 
-export default App
+export default App;
