7d1d2e4bb5a4b9add1091c635b15edaac2d155fe
Cory Walker <cory.walker.dev@gmail.com>
2023-09-21 11:14:24 -0700

    add hp bar flash

--- src/components/HpBar.jsx
+++ src/components/HpBar.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
+
 function HpBar({health}) {
+  const [flash, setFlash] = useState(false);
+
+  useEffect(() => {
+    if (health < 3) {
+      setFlash(true);
+      setTimeout(() => {
+        setFlash(false);
+      }, 500)
+    }
+  }, [health])
+
   return (
-    <div className="HpBar">HP: {health}</div>
+    <div className={`HpBar ${flash ? 'red' : ''}`}>HP: {health}</div>
   )
 }
 
